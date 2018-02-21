e9a3347293a36838fbab7b8698a7b2a6cb920696
Taylor C. Richberger <Taywee@gmx.com>
2018-02-21 12:12:51 -0700

    bump version for documentation and releases

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nestools
-A set of Rust tools used to help make NES games
+A set of Rust tools used to help make NES games.  The documentation is generated
+from the code, and can be found on the [docs.rs page](https://docs.rs/nestools).
+Releases are in the github releases.
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@
 //!
 //! All binaries are individually described in their own binary modules.  [The list of binary
 //! modules is namespaced for convenience](binaries/index.html).
+//!
+//! To download releases, you can either use a standard `cargo install`, or you can visit the
+//! [GitHub releases page](https://github.com/Taywee/nestools/releases).  I'll do my best to
+//! support a standard set of targets, but I can't make strong guarantees, as I do all of my
+//! development on my Linux machine.
 
 #[macro_use]
 extern crate serde_derive;
