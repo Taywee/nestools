49fe12c285117d98c893f454f3b3a815a6cbd3e7
Taylor C. Richberger <Taywee@gmx.com>
2018-05-09 23:45:41 -0600

    set up more stagec serialization

--- src/stage/serialize.rs
+++ src/stage/serialize.rs
@@ -2,18 +2,29 @@
 //! files for export as compressed binary stages.
 
 use std::collections::HashMap;
+use std::default::Default;
 
 /// Orientation enum for setting orientation
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
 pub enum Orientation {
+    #[serde(rename = "horizontal")]
     Horizontal,
+    #[serde(rename = "vertical")]
     Vertical,
+}
+
+impl Default for Orientation {
+    fn default() -> Orientation {
+        Orientation::Horizontal
+    }
 }
 
 /// Top level stage sheet type.  Holds all the data necessary to compile the stage file.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Stage {
     /// Whether the stage is serialized to be a horizontal stage or a vertical one
+    #[serde(default)]
     pub orientation: Orientation,
 
     /// Metatiles, for specifying the sprites used to compose a tile
@@ -22,3 +33,4 @@ pub struct Stage {
     /// The number of tiles to generate
     pub data: String,
 }
+
